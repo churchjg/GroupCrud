d9b1f0a67665a1215906e79a14d4744e97a58b8e
Ruben Rivera <rerivera76@gmail.com>
2020-11-18 08:19:43 -0500

    added log in log out sign up fucntions

--- movies/src/Components/Forms/LoginForm.js
+++ movies/src/Components/Forms/LoginForm.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button'
 import { withRouter } from 'react-router-dom'
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+import axios from 'axios';
 
 export class LoginForm extends Component {
     constructor(props) {
@@ -13,7 +14,69 @@ export class LoginForm extends Component {
             , dropdown: "",
             sign: false,
             login: false,
+            email: '',
+            password: '',
+            isLoggedIn: false
         }
+
+        this.handleLogOut = this.handleLogOut.bind(this)
+        this.handleInput = this.handleInput.bind(this)
+        this.handleLogIn = this.handleLogIn.bind(this)
+        this.handleSignUp = this.handleSignUp.bind(this)
+    }
+
+    componentDidMount() {
+        if (localStorage.token) {
+            this.setState({
+                isLoggedIn: true
+            })
+        } else {
+            this.setState({
+                isLoggedIn: false
+            })
+        }
+    }
+
+    handleLogOut() {
+        this.setState({
+            email: '',
+            password: '',
+            isLoggedIn: false
+        })
+        localStorage.clear()
+    }
+
+    handleInput(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSignUp(e) {
+        e.preventDefault()
+        axios.post('http://localhost:4000/auth/signup', {
+            headers: { "Content-Type": "application/json" },
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(response => {
+                localStorage.token = response.data.token
+                this.setState({ isLoggedIn: true })
+            })
+            .catch(err => console.log(err))
+    }
+
+    handleLogIn(e) {
+        e.preventDefault()
+        axios.post('http://localhost:52458/auth/login', {
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(response => {
+                localStorage.token = response.data.token
+                this.setState({ isLoggedIn: true })
+            })
+            .catch(err => console.log(err))
     }
 
     inputChange = (e) => {
@@ -61,27 +124,46 @@ export class LoginForm extends Component {
         return (
             <>
                 <Form inline onSubmit={this.submitForm}>
-                    <Button style={{ margin: 5, fontWeight: "bold" , backgroundColor: "#F5F5F5", color: "red", border: "2px solid red" }} size="md" type="submit" onClick={this.onOpenModal} >Sign Up</Button>
-                    <Button classname="login" style={{ margin: 5, fontWeight: "bold" , backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red" }} size="md" type="submit" onClick={this.onOpenModalLogin} >Login</Button>
-                    <Button style={{ margin: 5, fontWeight: "bold" , backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red", marginRight: "125px" }} size="md" type="submit" >Logout</Button>
+                    <Button style={{ margin: 5, fontWeight: "bold", backgroundColor: "#F5F5F5", color: "red", border: "2px solid red" }} size="md" type="submit" onClick={this.onOpenModal} >Sign Up</Button>
+                    <Button style={{ margin: 5, fontWeight: "bold", backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red" }} size="md" type="submit" onClick={this.onOpenModalLogin} >Login</Button>
+                    <Button style={{ margin: 5, fontWeight: "bold", backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red", marginRight: "125px" }} size="md" type="submit" onClick={this.handleLogOut} >Logout</Button>
                 </Form>
 
                 <Modal open={sign} onClose={this.onCloseModal}>
                     <div className="modal-body">
                         <h2>Get Started Absolutely<span> Free!</span></h2>
-                        <form className="contact-form form-validate3" novalidate="novalidate">
-                            <div className="form-group">
-                                <input className="form-control" type="text" name="name" id="name" placeholder="First Name" required="" autocomplete="off" aria-required="true" />
+                        <form>
+                            <div className="form-group text-left">
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email"
+                                    onChange={this.handleInput}
+                                    className="form-control"
+                                    id="email"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Enter email"
+                                />
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
-                            <div className="form-group">
-                                <input className="form-control" type="email" name="email" placeholder="E-mail" required="" autocomplete="off" aria-required="true" />
+                            <div className="form-group text-left">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password"
+                                    onChange={this.handleInput}
+                                    className="form-control"
+                                    id="password"
+                                    placeholder="Password"
+                                />
                             </div>
-                            <div className="form-group">
-                                <input type="password" name="pass" className="form-control" placeholder="Password" required="" autocomplete="off" aria-required="true" />
-                            </div>
-                            <input className="btn btn-md btn-primary btn-center" id="sign_up" type="button" value="Sign Up" />
+
+                            <button
+                                type="submit"
+                                onClick={this.handleSignUp}
+                                className="btn btn-primary"
+                            >
+                                Register
+                </button>
                         </form>
                     </div>
+
                 </Modal>
 
                 {/* <!-- signUp End -->
@@ -92,14 +174,35 @@ export class LoginForm extends Component {
                     <div className="modal-body">
                         <h2>Login and Get <span>Started</span></h2>
                         <span className="subtitle">Just fill in the form below</span>
-                        <form className="contact-form form-validate4" novalidate="novalidate">
-                            <div className="form-group">
-                                <input className="form-control" type="email" name="email" placeholder="E-mail" required="" autocomplete="off" aria-required="true" />
+                        <form>
+                            <div className="form-group text-left">
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email"
+                                    onChange={this.handleInput}
+                                    className="form-control"
+                                    id="email"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Enter email"
+                                />
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
-                            <div className="form-group">
-                                <input type="password" name="pass" className="form-control" placeholder="Password" required="" autocomplete="off" aria-required="true" />
+                            <div className="form-group text-left">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password"
+                                    onChange={this.handleInput}
+                                    className="form-control"
+                                    id="password"
+                                    placeholder="Password"
+                                />
                             </div>
-                            <input className="btn btn-md btn-primary btn-center" id="login_btn" type="button" value="Login" />
+
+                            <button
+                                type="submit"
+                                onClick={this.handleLogIn}
+                                className="btn btn-primary"
+                            >
+                                Log in
+                </button>
                         </form>
                     </div>
                 </Modal>
