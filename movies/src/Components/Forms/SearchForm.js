aa41c5bb7a73d4fbb9217b0e87128a90ced9b340
Jon <churchjg283@gmail.com>
2020-11-17 22:17:10 -0500

    css changes

--- movies/src/Components/Forms/SearchForm.js
+++ movies/src/Components/Forms/SearchForm.js
@@ -1,38 +1,59 @@
 import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import { withRouter } from 'react-router-dom'
-import TitleContainer from '../Homepage/TitleContainer'
+import Suggestions from './Suggestions'
+import { Col, Row, Container } from 'react-bootstrap'
+
+
+let url = "https://mernmovies.herokuapp.com/api/collections"
+
 
 export class SearchForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
             titles: []
-            , searchTerm: ''
+            , query: ''
         }
     }
 
-    editSearchTerm = (e) => {
-        this.setState({ searchTerm: e.target.value })
+    fetchMovies = () => {
+        fetch(`${url}/`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    ready: true
+                    , titles: res
+                })
+            })
+
     }
 
-    dynamicSearch = () => {
-        return this.state.titles.filter(title => title.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        }, () => {
+            if (this.state.query && this.state.query.length > 1) {
+                if (this.state.query.length % 2 === 0) {
+                    this.fetchMovies()
+                }
+            } else if (!this.state.query) {
+            }
+        })
     }
 
     render() {
         return (
 
-            <Form inline onSubmit={this.submitForm}>
-
-                
-                <TitleContainer titles={this.dynamicSearch()} />
-
-                <Button style={{ margin: 5, backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red" }} size="md" type="submit" >Search</Button>
-                <Form.Control style={{ margin: 5, border: "1px solid black", alignItems: "left" }} value={this.state.searchTerm} onChange={this.editSearchTerm} placeholder='Search by Title' />
+            <Form style={{ alignItems: "center", textAlign: "center"}}>
+                <Row style={{ alignItems: "center", textAlign: "center" }}>
+                    <Form.Control style={{ border: "1px solid black", alignItems: "center" }} ref={input => this.search = input} onChange={this.handleInputChange} placeholder='Search by Title' />
+                    <Button style={{ marginTop: "5px", marginLeft: "100px", textAlign: "center", backgroundColor: "#F5F5F5", color: "#333333", border: "2px solid red" }} size="md" type="submit" >Search</Button>
+                    <Suggestions titles={this.state.titles} />
+                </Row>
             </Form>
         )
     }
 }
-export default withRouter(SearchForm)
+export default SearchForm
